5e7b032a7ce127a4b63f4113d6ebe099952412cd
Renato Nobrega <renatonobrega87@gmail.com>
2023-05-12 16:08:43 -0300

    implementando o endpoint para listar os pagamentos

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -1,9 +1,15 @@
 package br.com.alurafood.pagamentos.controller;
 
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.service.PagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -11,4 +17,9 @@ public class PagamentoController {
 
     @Autowired
     private PagamentoService service;
+
+    @GetMapping
+    public Page<PagamentoDto> listar(@PageableDefault(size = 10 ) Pageable paginacao){
+        return service.obterTodos(paginacao);
+    }
 }
