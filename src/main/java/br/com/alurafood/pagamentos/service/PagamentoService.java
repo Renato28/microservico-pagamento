bc21b10d52fca5d85e0b78dd93d2091417fc507c
Renato Nobrega <renatonobrega87@gmail.com>
2023-05-12 15:16:05 -0300

    implementando a classe PagamentoService

---
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -0,0 +1,12 @@
+package br.com.alurafood.pagamentos.service;
+
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository repository;
+}
