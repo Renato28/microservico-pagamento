0f216d8820764262eed5d3034bb7dc8ac37a333c
Renato Nobrega <renatonobrega87@gmail.com>
2023-05-12 15:22:25 -0300

    implementando metodo para obter todos os pagamentos e adicionando dependencia do ModelMapper no pom.xml

--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -1,7 +1,11 @@
 package br.com.alurafood.pagamentos.service;
 
+import br.com.alurafood.pagamentos.dto.PagamentoDto;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +13,14 @@ public class PagamentoService {
 
     @Autowired
     private PagamentoRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDto> obterTodos(Pageable paginacao){
+        return repository
+                .findAll(paginacao)
+                .map(pagamento -> modelMapper.map(pagamento, PagamentoDto.class));
+
+    }
 }
